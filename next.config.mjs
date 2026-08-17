80203bf3e16cafe8827199d002f2abd1eca2c7b0
lsanghak-steve <lsanghak@naver.com>
2026-08-17 22:44:18 +0900

    chore: Update next.config.mjs with allowedDevOrigins

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['*.trycloudflare.com', 'localhost:3000']
 };
 
 export default nextConfig;
